e91d9d14a3cd694929b16cdfcbbc8cf265bb1873
Yuki <yuki.mul.tiplus@gmail.com>
2017-09-19 21:26:16 +0900

    add TokenSpec which tests Token class

--- src/test/scala/token/TokenSpec.scala
+++ src/test/scala/token/TokenSpec.scala
@@ -1,5 +1,7 @@
 package token
 
+import org.scalatest.WordSpec
+
 /*
 * Copyright 2017 Yuki Toyoda
 *
@@ -16,6 +18,56 @@ package token
 * limitations under the License.
 */
 
-class TokenSpec {
+class TokenSpec extends WordSpec {
 
+  "NumberToken" should {
+    "return line value of 100" in {
+      val token = new NumberToken(100, 1)
+      assert(token.lineNumber == 100)
+    }
+
+    "return value value of 100" in {
+      val token = new NumberToken(1, 100)
+      assert(token.getValue == 100)
+    }
+
+    "return isNumber to be true" in {
+      val token = new NumberToken(1, 1)
+      assert(token.isNumber)
+    }
+  }
+
+  "StringToken" should {
+    "return line value of 100" in {
+      val token = new StringToken(100, "")
+      assert(token.lineNumber == 100)
+    }
+
+    "return literal value of \"AAA\"" in {
+      val token = new StringToken(100, "AAA")
+      assert(token.getText == "AAA")
+    }
+
+    "return isString to be true" in {
+      val token = new StringToken(1, "")
+      assert(token.isString)
+    }
+  }
+
+  "IdentifierToken" should {
+    "return line value of 100" in {
+      val token = new IdentifierToken(100, "")
+      assert(token.lineNumber == 100)
+    }
+
+    "return id value of a123bcd" in {
+      val token = new IdentifierToken(1, "a123bcd")
+      assert(token.getText == "a123bcd")
+    }
+
+    "return isIdentifier to be true" in {
+      val token = new IdentifierToken(1, "")
+      assert(token.isIdentifier)
+    }
+  }
 }
